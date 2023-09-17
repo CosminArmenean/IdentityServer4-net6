b7f13e139fc06ec6db0f491367ff25d78a90a0fd
CosminArmenean <cosmin.armenean@gmail.com>
2023-09-17 23:22:56 +0300

    using mysql

--- src/EntityFramework/host/Startup.cs
+++ src/EntityFramework/host/Startup.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
@@ -26,24 +26,44 @@ namespace IdentityServerHost
             var connectionString = _config.GetConnectionString("db");
 
             services.AddIdentityServer()
-                .AddDeveloperSigningCredential()
-                .AddTestUsers(TestUsers.Users)
-                // this adds the config data from DB (clients, resources, CORS)
-                .AddConfigurationStore(options =>
-                {
-                    options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString);
-                })
-                // this adds the operational data from DB (codes, tokens, consents)
-                .AddOperationalStore(options =>
-                {
-                    options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString);
+               .AddDeveloperSigningCredential()
+               .AddTestUsers(TestUsers.Users)
+               // this adds the config data from DB (clients, resources, CORS)
+               .AddConfigurationStore(options =>
+               {
+                   options.ConfigureDbContext = builder => builder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+               })
+               // this adds the operational data from DB (codes, tokens, consents)
+               .AddOperationalStore(options =>
+               {
+                   options.ConfigureDbContext = builder => builder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 
-                    // this enables automatic token cleanup. this is optional.
-                    options.EnableTokenCleanup = true;
-                    options.TokenCleanupInterval = 5; // interval in seconds, short for testing
-                });
-                // this is something you will want in production to reduce load on and requests to the DB
-                //.AddConfigurationStoreCache();
+                   // this enables automatic token cleanup. this is optional.
+                   options.EnableTokenCleanup = true;
+                   options.TokenCleanupInterval = 5; // interval in seconds, short for testing
+               });
+            // this is something you will want in production to reduce load on and requests to the DB
+            //.AddConfigurationStoreCache();
+
+            //services.AddIdentityServer()
+            //    .AddDeveloperSigningCredential()
+            //    .AddTestUsers(TestUsers.Users)
+            //    // this adds the config data from DB (clients, resources, CORS)
+            //    .AddConfigurationStore(options =>
+            //    {
+            //        options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString);
+            //    })
+            //    // this adds the operational data from DB (codes, tokens, consents)
+            //    .AddOperationalStore(options =>
+            //    {
+            //        options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString);
+
+            //        // this enables automatic token cleanup. this is optional.
+            //        options.EnableTokenCleanup = true;
+            //        options.TokenCleanupInterval = 5; // interval in seconds, short for testing
+            //    });
+            //    // this is something you will want in production to reduce load on and requests to the DB
+            //    //.AddConfigurationStoreCache();
         }
 
         public void Configure(IApplicationBuilder app)
