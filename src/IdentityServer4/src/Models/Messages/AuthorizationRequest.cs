5f5efa90b2778b5831c9b3eb474d8b751cef9ca3
CosminArmenean <cosmin.armenean@gmail.com>
2023-09-17 13:02:47 +0300

    build identityserver4 Added storage reference

--- src/IdentityServer4/src/Models/Messages/AuthorizationRequest.cs
+++ src/IdentityServer4/src/Models/Messages/AuthorizationRequest.cs
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
-using IdentityServer4.EntityFramework.Entities;
+
 using IdentityServer4.Validation;
 using System.Collections.Generic;
 using System.Collections.Specialized;
